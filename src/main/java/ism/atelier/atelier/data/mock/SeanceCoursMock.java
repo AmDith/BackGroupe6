7e02e8dc02bccbfb0ed45cf5026da013661a8cc7
AmDith <exauceeyemandoko@gmail.com>
2025-06-16 12:31:26 +0100

    DSD

--- src/main/java/ism/atelier/atelier/data/mock/SeanceCoursMock.java
+++ src/main/java/ism/atelier/atelier/data/mock/SeanceCoursMock.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-//@Order(11)
-//@Component
+@Order(11)
+@Component
 @RequiredArgsConstructor
 public class SeanceCoursMock implements CommandLineRunner {
     private final SeanceCoursRepository seanceCoursRepository;
@@ -21,33 +21,33 @@ public class SeanceCoursMock implements CommandLineRunner {
     public void run(String... args) throws Exception {
         List<SeanceCours> seanceCours = seanceCoursRepository.findAll();
         if (seanceCours.isEmpty()) {
-//            SeanceCours seanceCours1 = new SeanceCours();
-//            seanceCours1.setId("1");
-//            seanceCours1.setDate(LocalDate.parse("2025-06-13"));
-//            seanceCours1.setHeureDb(LocalTime.of(15, 50));
-//            seanceCours1.setHeureFin(LocalTime.of(15, 59));
-//            seanceCours1.setNow(false);
-//            seanceCours1.setCoursId("1");
-//            seanceCours.add(seanceCours1);
-//
-//            SeanceCours seanceCours2 = new SeanceCours();
-//            seanceCours2.setId("2");
-//            seanceCours2.setDate(LocalDate.parse("2025-06-13"));
-//            seanceCours2.setHeureDb(LocalTime.of(15, 50));
-//            seanceCours2.setHeureFin(LocalTime.of(15, 59));
-//            seanceCours2.setNow(false);
-//            seanceCours2.setCoursId("1");
-//            seanceCours.add(seanceCours2);
-//
-//
-//            SeanceCours seanceCours3 = new SeanceCours();
-//            seanceCours3.setId("3");
-//            seanceCours3.setDate(LocalDate.parse("2025-06-13"));
-//            seanceCours3.setHeureDb(LocalTime.of(15, 50));
-//            seanceCours3.setHeureFin(LocalTime.of(15, 59));
-//            seanceCours3.setNow(false);
-//            seanceCours3.setCoursId("2");
-//            seanceCours.add(seanceCours3);
+            SeanceCours seanceCours1 = new SeanceCours();
+            seanceCours1.setId("1");
+            seanceCours1.setDate(LocalDate.parse("2025-06-16"));
+            seanceCours1.setHeureDb(LocalTime.of(12, 40));
+            seanceCours1.setHeureFin(LocalTime.of(13, 30));
+            seanceCours1.setNow(false);
+            seanceCours1.setCoursId("1");
+            seanceCours.add(seanceCours1);
+
+            SeanceCours seanceCours2 = new SeanceCours();
+            seanceCours2.setId("2");
+            seanceCours2.setDate(LocalDate.parse("2025-06-16"));
+            seanceCours2.setHeureDb(LocalTime.of(15, 50));
+            seanceCours2.setHeureFin(LocalTime.of(15, 59));
+            seanceCours2.setNow(false);
+            seanceCours2.setCoursId("1");
+            seanceCours.add(seanceCours2);
+
+
+            SeanceCours seanceCours3 = new SeanceCours();
+            seanceCours3.setId("3");
+            seanceCours3.setDate(LocalDate.parse("2025-06-16"));
+            seanceCours3.setHeureDb(LocalTime.of(12, 40));
+            seanceCours3.setHeureFin(LocalTime.of(13, 30));
+            seanceCours3.setNow(false);
+            seanceCours3.setCoursId("2");
+            seanceCours.add(seanceCours3);
 //
 //            SeanceCours seanceCours4 = new SeanceCours();
 //            seanceCours4.setId("4");
